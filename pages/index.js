7acc92e69cc400b7677d1f056425f14284699590
Aswin Asok <aswinasokofficial@gmail.com>
2021-10-18 15:44:52 +0530

    Instagram Clone Started

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1> This is the Instagram Build </h1>
+
+      
+    </div>
+  );
+}
