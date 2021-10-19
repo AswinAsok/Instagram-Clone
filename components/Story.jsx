97c7d44c7892861b67d575bd6da61f6fbaf16a06
Aswin Asok <aswinasokofficial@gmail.com>
2021-10-19 14:53:48 +0530

    Stories Styling has been done

--- components/Story.jsx
+++ components/Story.jsx
@@ -1,11 +1,14 @@
-const Story = ({img, username}) => {
-    return (
-        <div>
-            <img src={img} alt="" />
-            <p>{username}</p>
-            <p>Hello World</p>
-        </div>
-    )
-}
+const Story = ({ img, username }) => {
+  return (
+    <div>
+      <img
+        className="h-14 w-14 rounded-full p-[1.5px] border-red-500 border-2 object-contain cursor-pointer hover:scale-110 transition transform druation-200 ease-out"
+        src={img}
+        alt=""
+      />
+      <p className="text-xs w-14 truncate text-center">{username}</p>
+    </div>
+  );
+};
 
-export default Story
+export default Story;
