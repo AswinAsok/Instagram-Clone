8dc9600bcc9a8748780400f94247d068bb49e73d
Aswin Asok <aswinasokofficial@gmail.com>
2021-10-20 12:45:53 +0530

    Suggestions Section has been done

--- components/Suggestions.jsx
+++ components/Suggestions.jsx
@@ -1,9 +1,50 @@
-const Suggestions = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import { useState, useEffect } from "react";
+import faker from "faker";
 
-export default Suggestions
+const Suggestions = () => {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+
+    setSuggestions(suggestions);
+  }, []);
+  return (
+    <div className="mt-4 ml-10">
+      <div className="flex justify-between text-sm mb-5">
+        <h3 className="text-sm font-semi-bold text-gray-400">
+          Suggetions for you
+        </h3>
+        <button className="text-gray-600 font-semibold">See All</button>
+      </div>
+
+      {suggestions.map((profile) => (
+        <div
+          key={profile.id}
+          className="flex items-center justify-between mt-3"
+        >
+          <img
+            src={profile.avatar}
+            className="w-10 h-10 rounded-full p-[2px]"
+            alt=""
+          />
+
+          <div className="flex-1 m1-4">
+            <h2 className="font-semibold text-sm ">{profile.username}</h2>
+            <h3 className="text-xs text-gray-400">
+              Works at {profile.company.name}
+            </h3>
+          </div>
+          <button className="text-xs font-semibold text-blue-400">
+            Follow
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Suggestions;
